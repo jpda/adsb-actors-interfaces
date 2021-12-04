742ff756533275d535ad94ed00ad07b188a97869
jpda <john.dandison@live.com>
2021-12-03 23:43:59 -0500

    initial commit

---
+++ IVehicleActor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Dapr.Actors;
+
+namespace Adsb.Actors.Interfaces
+{
+    public interface IVehicleActor : IActor
+    {
+        Task SetPositionAsync(Position positionData);
+        Task<Position> GetPositionAsync();
+    }
+
+    public class Position
+    {
+        public Position() { }
+        public Position(decimal latitude, decimal longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+    }
+}
